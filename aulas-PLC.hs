ed73fbd13811806d9c89c58af99efff5200d5fd3
Thiago Costa <104111765+Strateago@users.noreply.github.com>
2023-11-09 20:03:01 -0300

    aulas-PLC

--- aulas-PLC.hs
+++ aulas-PLC.hs
@@ -1,3 +1,4 @@
+-- AULA 1 -----------------------------------------------------------------------------------------------------------
 vendas :: Int -> Int
 vendas 0 = 30
 vendas 1 = 50
@@ -16,7 +17,8 @@ maxVendas n | n == 0 = vendas 0
 totalVendas :: Int -> Int
 totalVendas n | n == 0 = vendas 0
               | otherwise = totalVendas (n-1) + vendas n
-
+        
+-- AULA 2 -------------------------------------------------------------------------------------------------------
 addEspacos :: Int -> String
 addEspacos 0 = ""
 addEspacos n = " " ++ addEspacos (n-1)
@@ -33,3 +35,35 @@ imprimeMedia n = "Media: " ++ show (fromIntegral (totalVendas n) / fromIntegral 
 
 imprimeTabela :: Int -> IO()
 imprimeTabela n = putStr ("Semana    Venda\n" ++ imprimeSemana n ++ "\n" ++ imprimeTotal n ++ imprimeMedia n)
+
+-- AULA 3 -----------------------------------------------------------------------------------------------------------
+lista1 :: [Int]
+lista1 = [1,2,3,4]
+
+lista2 :: String
+lista2 = "g2h31gb10jh57"
+
+lista3 :: [(Int,Int)]
+lista3 = [(2,3),(4,6),(1,6),(4,7)]
+
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (h:t) = h + sumList t
+
+double :: [Int] -> [Int]
+double [] = []
+double (h:t) = (2*h):(double t)
+
+member :: [Int] -> Int -> Bool
+member [] n = False
+member (h:t) n | n == h = True
+               | otherwise = member t n
+
+digits :: String -> String
+digits [] = []
+digits (h:t) | h <= '9' && h >= '0' = h:(digits t)
+             | otherwise = digits t
+
+sumPairs :: [(Int, Int)] -> [Int]
+sumPairs [] = []
+sumPairs ((x, y):t) = (x+y):(sumPairs t)
