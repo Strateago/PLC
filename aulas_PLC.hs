55246fa671303b3547e43d17220e7cf93f2f3e75
Thiago Costa <104111765+Strateago@users.noreply.github.com>
2023-11-09 20:22:53 -0300

    aulas_PLC

---
+++ aulas_PLC.hs
@@ -0,0 +1,82 @@
+-- AULA 1 -----------------------------------------------------------------------------------------------------------
+vendas :: Int -> Int
+vendas 0 = 30
+vendas 1 = 50
+vendas 2 = 80
+vendas 3 = 60
+vendas 4 = 50
+vendas 5 = 50
+vendas 6 = 150
+vendas 7 = 50
+vendas n = 0
+
+maxVendas :: Int -> Int
+maxVendas n | n == 0 = vendas 0
+            | otherwise = max (maxVendas (n-1)) (vendas n)
+
+totalVendas :: Int -> Int
+totalVendas n | n == 0 = vendas 0
+              | otherwise = totalVendas (n-1) + vendas n
+
+vendasIgualA :: Int -> Int -> Int
+vendasIgualA s 0 | vendas 0 == s = 1
+              | otherwise = 0
+vendasIgualA s n | vendas n == s = 1 + vendasIgualA s (n-1)
+              | otherwise = vendasIgualA s (n-1)
+
+ehPrimo :: Int -> Bool
+ehPrimo 2 = True
+ehPrimo n = modl n (div n 2 + 1)
+    where modl x 2 = mod x 2 == 1
+          modl x y | mod x y == 0 = False
+                   | otherwise = modl x (y-1)
+        
+-- AULA 2 -------------------------------------------------------------------------------------------------------
+addEspacos :: Int -> String
+addEspacos 0 = ""
+addEspacos n = " " ++ addEspacos (n-1)
+
+imprimeSemana :: Int -> String
+imprimeSemana 0 = "  0" ++ addEspacos 8 ++ show (vendas 0)
+imprimeSemana n = imprimeSemana (n-1) ++ "\n  " ++ show n ++  addEspacos 8 ++ show (vendas n)
+
+imprimeTotal :: Int -> String
+imprimeTotal n = "Total: " ++ show (totalVendas n) ++ "\n"
+
+imprimeMedia :: Int -> String
+imprimeMedia n = "Media: " ++ show (fromIntegral (totalVendas n) / fromIntegral (n+1)) ++ "\n"
+
+imprimeTabela :: Int -> IO()
+imprimeTabela n = putStr ("Semana    Venda\n" ++ imprimeSemana n ++ "\n" ++ imprimeTotal n ++ imprimeMedia n)
+
+-- AULA 3 -----------------------------------------------------------------------------------------------------------
+lista1 :: [Int]
+lista1 = [1,2,3,4]
+
+lista2 :: String
+lista2 = "g2h31gb10jh57"
+
+lista3 :: [(Int,Int)]
+lista3 = [(2,3),(4,6),(1,6),(4,7)]
+
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (h:t) = h + sumList t
+
+double :: [Int] -> [Int]
+double [] = []
+double (h:t) = (2*h):(double t)
+
+member :: [Int] -> Int -> Bool
+member [] n = False
+member (h:t) n | n == h = True
+               | otherwise = member t n
+
+digits :: String -> String
+digits [] = []
+digits (h:t) | h <= '9' && h >= '0' = h:(digits t)
+             | otherwise = digits t
+
+sumPairs :: [(Int, Int)] -> [Int]
+sumPairs [] = []
+sumPairs ((x, y):t) = (x+y):(sumPairs t)
