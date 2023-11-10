88e2a0e7cc7a4830a602d8ddac3b00a0bd6f6d5e
Thiago Costa <104111765+Strateago@users.noreply.github.com>
2023-11-09 22:52:22 -0300

    Update aulas_PLC.hs

--- aulas_PLC.hs
+++ aulas_PLC.hs
@@ -1,4 +1,14 @@
 -- AULA 1 -----------------------------------------------------------------------------------------------------------
+square :: Int -> Int
+square x = x * x
+
+allEqual :: Int -> Int -> Int -> Bool
+allEqual n m p = (n == m) && (m == p)
+
+maxi :: Int -> Int -> Int
+maxi n m | n >= m = n
+         | otherwise = m
+
 vendas :: Int -> Int
 vendas 0 = 30
 vendas 1 = 50
@@ -12,7 +22,7 @@ vendas n = 0
 
 maxVendas :: Int -> Int
 maxVendas n | n == 0 = vendas 0
-            | otherwise = max (maxVendas (n-1)) (vendas n)
+            | otherwise = maxi (maxVendas (n-1)) (vendas n)
 
 totalVendas :: Int -> Int
 totalVendas n | n == 0 = vendas 0
@@ -30,7 +40,25 @@ ehPrimo n = modl n (div n 2 + 1)
     where modl x 2 = mod x 2 == 1
           modl x y | mod x y == 0 = False
                    | otherwise = modl x (y-1)
-        
+
+primosEntreSi :: Int -> Int -> Bool
+primosEntreSi x y | x > y = mdc x y == 1
+                  | y > x = mdc y x == 1
+    where mdc a b | mod a b == 0 = b
+                  | otherwise = mdc b (mod a b)
+
+fat :: Int -> Int
+fat 0 = 1
+fat n = n * fat (n-1)
+
+all4Equal :: Int -> Int -> Int -> Int -> Bool
+all4Equal a b c d = a == b && allEqual b c d
+
+equalCount :: Int -> Int -> Int -> Int
+equalCount a b c | allEqual a b c = 3
+                 | a == b || a == c || b == c = 2
+                 | otherwise = 0
+
 -- AULA 2 -------------------------------------------------------------------------------------------------------
 addEspacos :: Int -> String
 addEspacos 0 = ""
@@ -80,3 +108,5 @@ digits (h:t) | h <= '9' && h >= '0' = h:(digits t)
 sumPairs :: [(Int, Int)] -> [Int]
 sumPairs [] = []
 sumPairs ((x, y):t) = (x+y):(sumPairs t)
+
+-- AULA 4 -----------------------------------------------------------------------------------------------------------
