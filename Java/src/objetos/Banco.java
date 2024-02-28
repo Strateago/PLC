d8e036744057f71dce88ef9ac49afc63714c04f1
Thiago Costa <104111765+Strateago@users.noreply.github.com>
2024-02-28 16:40:34 -0300

    Add files via upload

--- Java/src/objetos/Banco.java
+++ Java/src/objetos/Banco.java
@@ -2,39 +2,20 @@ package objetos;
 
 public class Banco {
     private RepositorioContas accounts;
-    private int indice;
     private double taxa_juros;
 
     public Banco(RepositorioContas rep){
-        this.accounts = new RepositorioContasArray();
-        this.indice = 0;
+        this.accounts = rep;
         this.taxa_juros = 0.01;
     }
     public void cadastrarContaAbstrata(ContaAbstrata c){
-        accounts[indice] = c;
-        this.indice++;
+        accounts.inserir(c);
     }
     public void creditar(String n, double v){
-        procurarContaAbstrata(n).creditar(v);
-    }
-    private ContaAbstrata procurarContaAbstrata(String numero) {
-        int i = 0;
-        boolean achou = false;
-        ContaAbstrata resposta = null;
-        while ((! achou) && (i < indice)) {
-            if (accounts[i].getNumero().equals(numero)) {
-                achou = true;
-                resposta = accounts[i];
-            } 
-            else {
-                i = i + 1;
-            } 
-        }
-        if (!achou) throw new RuntimeException("Não achou a contaAbstrContaAbstrata");
-        return resposta;
+        accounts.procurar(n).creditar(v);
     }
     public void renderJuros(String numero){
-        ContaAbstrata c = procurarContaAbstrata(numero);
+        ContaAbstrata c = accounts.procurar(numero);
         if(c instanceof Poupança){
             ((Poupança)c).renderJuros(this.taxa_juros);
         }
@@ -43,7 +24,7 @@ public class Banco {
         }
     }
     public void renderBonus(String numero){
-        ContaAbstrata c = procurarContaAbstrata(numero);
+        ContaAbstrata c = accounts.procurar(numero);
         if(c instanceof Conta_Esp){
             ((Conta_Esp)c).renderBonus();
         }
@@ -53,8 +34,8 @@ public class Banco {
     }
     public void transferir(String n1, String n2, double valor){ // n1 transfere e n2 recebe
         ContaAbstrata c1, c2;
-        c1 = procurarContaAbstrata(n1);
-        c2 = procurarContaAbstrata(n2);
+        c1 = accounts.procurar(n1);
+        c2 = accounts.procurar(n2);
         c1.debitar(valor);
         c2.creditar(valor);
     }
--- Java/src/objetos/RepositorioContasArray.java
+++ Java/src/objetos/RepositorioContasArray.java
@@ -1,23 +1,59 @@
 package objetos;
 
 public class RepositorioContasArray implements RepositorioContas{
-    public RepositorioContasArray(){
-
+    private ContaAbstrata[] c;
+    private int indice;
+    private int i;
+    public RepositorioContasArray(int num){
+        this.c = new ContaAbstrata[num];
+        this.indice = 0;
     }
-    void inserir(ContaAbstrata c){
-
+    public void inserir(ContaAbstrata c){
+        this.c[indice] = c;
+        indice++;
     }
-    ContaAbstrata procurar(String s){
-
+    public ContaAbstrata procurar(String s){
+        i = 0;
+        boolean achou = false;
+        ContaAbstrata resposta = null;
+        while ((! achou) && (i < indice)) {
+            if (c[i].getNumero().equals(s)) {
+                achou = true;
+                resposta = c[i];
+            } 
+            else {
+                i = i + 1;
+            } 
+        }
+        if (!achou) throw new RuntimeException("Não achou a contaAbstrContaAbstrata");
+        return resposta;
     }
-    void remover(String s){
-
+    public void remover(String s){
+        if(existe(s)){
+            indice--;
+            for(; i < indice; i++){
+                c[i] = c[i+1];
+            }
+        }
+        else{
+            throw new RuntimeException("A conta não existe");
+        }
     }
-    void atualizar(ContaAbstrata c){
-
-    }
-    boolean existe(String s){
+    public void atualizar(ContaAbstrata c){
         
+    }
+    public boolean existe(String s){
+        i = 0;
+        boolean achou = false;
+        while ((! achou) && (i < indice)) {
+            if (c[i].getNumero().equals(s)) {
+                achou = true;
+            } 
+            else {
+                i = i + 1;
+            } 
+        }
+        return achou;
     }
 
 }
